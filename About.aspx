7eb676f6896544a500814d287cb8fa97cff2ab4e
Alex <alexbollen@umes.edu.gt>
2023-06-07 22:24:26 -0600

    Edicion de albumes

--- About.aspx
+++ About.aspx
@@ -1,7 +1,23 @@
 ﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="PracticaFinal.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
-    <h3>Your application description page.</h3>
-    <p>Use this area to provide additional information.</p>
+    <h2 align="center">EDICION ALBÚM</h2>
+    <h3>Ingrese nombre del album a editar:
+        <asp:TextBox ID="TextBoxBuscarAlbum" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="ButtonBuscar" runat="server" OnClick="ButtonBuscar_Click" Text="BUSCAR" />
+&nbsp;<asp:Label ID="LabelEstado" runat="server" Text="[ESTADO]"></asp:Label>
+    </h3>
+        <p class="lead">Titulo de albúm:
+            <asp:TextBox ID="TextBoxTituloAlbum" runat="server" Enabled="False"></asp:TextBox>
+        </p>
+        <p class="lead">Artista del albúm:
+            <asp:TextBox ID="TextBoxArtistaAlbum" runat="server" Enabled="False"></asp:TextBox>
+        </p>
+        <p class="lead">Fecha publicación:
+            <asp:TextBox ID="TextBoxFechaPublicacion" runat="server" Enabled="False"></asp:TextBox>
+        </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ButtonEditar" runat="server" Enabled="False" Height="42px" OnClick="ButtonEditar_Click" Text="EDITAR" Width="99px" />
+            &nbsp;</p>
 </asp:Content>
--- About.aspx.cs
+++ About.aspx.cs
@@ -10,5 +10,45 @@ namespace PracticaFinal {
         protected void Page_Load(object sender, EventArgs e) {
 
         }
+
+        protected void ButtonBuscar_Click(object sender, EventArgs e) {
+            Archivo archivo = new Archivo();
+            List<Album> albumes = archivo.Leer();
+            Album albumExiste = albumes.Find(a => a.TituloAlbum == TextBoxBuscarAlbum.Text);
+
+            if (albumExiste != null) {
+                Response.Write("<script>alert('Albúm Encontrado.')</script>");
+                LabelEstado.Text = albumExiste.TituloAlbum.ToString();
+                ButtonEditar.Enabled = true;
+                TextBoxArtistaAlbum.Enabled = true;
+                TextBoxTituloAlbum.Enabled = true;
+                TextBoxFechaPublicacion.Enabled = true;
+
+                TextBoxTituloAlbum.Text = albumExiste.TituloAlbum;
+                TextBoxArtistaAlbum.Text = albumExiste.ArtistaAlbum;
+                TextBoxFechaPublicacion.Text = albumExiste.FechaPublicacion;
+            } else {
+                Response.Write("<script>alert('Albúm NO Encontrado.')</script>");
+            }
+        }
+
+        protected void ButtonEditar_Click(object sender, EventArgs e) {
+            Archivo archivo = new Archivo();
+            List<Album> albumes = archivo.Leer();
+            Album album = albumes.Find(u => u.TituloAlbum == LabelEstado.Text);
+            album.TituloAlbum = TextBoxTituloAlbum.Text;
+            album.ArtistaAlbum = TextBoxArtistaAlbum.Text;
+            album.FechaPublicacion = TextBoxFechaPublicacion.Text;
+
+            Response.Write("<script>alert('Datos de albúm actualizados.')</script>");
+            archivo.Grabar(albumes);
+
+            TextBoxBuscarAlbum.Text = "";
+            ButtonBuscar.Enabled = false;
+            TextBoxArtistaAlbum.Enabled = false;
+            TextBoxTituloAlbum.Enabled = false;
+            TextBoxFechaPublicacion.Enabled = false;
+            LabelEstado.Text = "[ESTADO]";
+        }
     }
 }
